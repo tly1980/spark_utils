4b3232abcafe162b93c4c75674ab92ac170c7593
Tom Tang <tly1980@gmail.com>
2015-06-23 16:36:53 +1000

    misc good changes

--- src/spark_utils/sqle.py
+++ src/spark_utils/sqle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import argparse
 import logging
+import os
 
 
 AP = argparse.ArgumentParser(description="Spark SQL executor")
-AP.add_argument('sql', nargs='+')
+AP.add_argument('src', nargs='+')
 AP.add_argument('-x', nargs='+', type=str, default=[], help="Override the permissions.")
-AP.add_argument('--show', action='store_true', default=False, help="Dry run")
+AP.add_argument('--quiet', action='store_false', dest='show', help='Not show')
 AP.add_argument('--dry', action='store_true', default=False, help="Dry run")
-AP.add_argument('--no-hive', action='store_true', default=False, help="Use SqlContext rather than HiveContext.")
+AP.add_argument('--nohive', action='store_true', default=False, help="Use SqlContext rather than HiveContext.")
 
 
 logging.basicConfig(
@@ -44,6 +45,49 @@ class SQLRunner(object):
             df.show()
         return df
 
+
+class SQLRender(object):
+    def __init__(self, srcs, params):
+        self.srcs = srcs
+        self.params = params
+
+    def next_stmt(self):
+        for s in self.srcs:
+            for stmt in self.load(s):
+                yield stmt
+
+    def load(self, src):
+        if src[0] in ['.', '/']:
+            # this means src could be either a file or directory
+            if not os.path.isdir(src):
+                for stmt in x_file(src, self.params):
+                    yield (stmt, src)
+            else:
+                for root, dirs, files in os.walk(src):
+                    for f in sorted(files):
+                        path = os.path.join(root, f)
+                        for stmt in x_file(path, self.params):
+                            yield (stmt, path)
+
+        else:
+            src = src.format(**self.params)
+            for stmt in x_sql_stmt(src):
+                yield stmt, '<FROM ARGS>'
+
+
+def x_sql_stmt(cnt):
+    for stmt in cnt.split(';'):
+        stmt2 = stmt.strip()
+        if stmt2:
+            yield stmt2
+
+def x_file(src_file, params):
+    with open(src_file) as f:
+        cnt = f.read().format(**params)
+        cnt = cnt.strip()
+        return x_sql_stmt(cnt)
+
+
 def x_params(x_pairs):
     return dict([ x.split('=') for x in x_pairs])
 
@@ -54,12 +98,6 @@ def x_sql_content(sql_str, params):
 
     return sql_str.format(**params)
 
-def x_sql_stmt(sql_contents):
-    for sql in sql_contents:
-        for stmt in sql.split(';'):
-            stmt2 = stmt.strip()
-            if stmt2:
-                yield stmt2
 
 
 def main(args):
@@ -68,9 +106,12 @@ def main(args):
         from pyspark.sql import SQLContext, HiveContext
 
         sc = SparkContext(appName=__file__)
-        if args['no-hive']:
+        if args.nohive:
+            logging.info('nohive is set')
+            logging.info('using SQLContext')
             sql_context = SQLContext(sc)
         else:
+            logging.info('using HiveContext')
             sql_context = HiveContext(sc)
     else:
         sql_context = None
@@ -78,9 +119,9 @@ def main(args):
     runner = SQLRunner(sql_context, args.show)
     params = x_params(args.x)
 
-    sql_cnts = [ x_sql_content(s, params) for s in args.sql ]
+    render = SQLRender(args.src, params)
 
-    for stmt in x_sql_stmt(sql_cnts):
+    for stmt in render.next_stmt():
         runner.run_sql(stmt)
 
 
